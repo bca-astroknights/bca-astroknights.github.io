18f2cb69f88347093372a146a1d1e3fb963bdb2a
kna27 <krisharora27@gmail.com>
2021-10-30 12:50:52 -0400

    Add support for comments in bloglist + duplicates

--- fetchBlogs.js
+++ fetchBlogs.js
@@ -1,16 +1,33 @@
+var links = [];
+
 fetch("./bloglist.txt")
     .then(x => x.text())
     .then(data => {
-        data.replace(/\r\n/g, "\n").split("\n").forEach(element => appendBlog(element));
+        links = data.replace(/\r\n/g, "\n").split("\n");
+        links = cleanLinks(links);
+        links.forEach(element => appendBlog(element));
     });
 
+function cleanLinks(links) {
+    for (let i = 0; i < links.length; i++) {
+        if (links[i][0] == "#") {
+            delete links[i];
+        }
+    }
+    links = [...new Set(links)];
+
+    return links;
+}
+
 function appendBlog(blogFile) {
-    let tag = document.createElement("a");
-    let title = blogFile.replace(/^.*[\\\/]/, "").replace(/\.[^/.]+$/, "");
-    let text = document.createTextNode(title);
-    tag.appendChild(text);
-    tag.href = blogFile;
-    let element = document.getElementById("blogsContainer");
-    element.appendChild(tag);
-    element.appendChild(document.createElement("br"));
+    if (blogFile) {
+        let tag = document.createElement("a");
+        let title = blogFile.replace(/^.*[\\\/]/, "").replace(/\.[^/.]+$/, "");
+        let text = document.createTextNode(title);
+        tag.appendChild(text);
+        tag.href = blogFile;
+        let element = document.getElementById("blogsContainer");
+        element.appendChild(tag);
+        element.appendChild(document.createElement("br"));
+    }
 }
